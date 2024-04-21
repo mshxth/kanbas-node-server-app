ad397e38f528ce429e8247fb84469978e1876481
Manav Sheth <122500006+mshxth@users.noreply.github.com>
2024-04-21 13:57:08 -0400

    commit

--- Kanbas/courses/routes.js
+++ Kanbas/courses/routes.js
@@ -20,7 +20,6 @@ export default function CourseRoutes(app) {
     res.sendStatus(204);
   });
 
-
   app.delete("/api/courses/:id", (req, res) => {
     const { id } = req.params;
     Database.courses = Database.courses.filter((c) => c._id !== id);
